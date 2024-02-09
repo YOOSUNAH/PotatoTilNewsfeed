35e419840bee5f2e458000c45bccd613466d21f3
haeun0405 <124241345+haeun0405@users.noreply.github.com>
2024-02-09 16:08:39 +0900

    :sparkles: til 게시글 삭제 기능 구현

--- src/main/java/com/example/potatotilnewsfeed/domain/til/controller/TilController.java
+++ src/main/java/com/example/potatotilnewsfeed/domain/til/controller/TilController.java
@@ -11,11 +11,11 @@ import java.net.URI;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -65,5 +65,11 @@ public class TilController {
 
         TilResponseDto responseDto = new TilResponseDto("TIL이 수정되었습니다.", List.of(tilData));
         return ResponseEntity.ok().body(responseDto);
+    }
+
+    @DeleteMapping("v1/tils/{tilId}")
+    public ResponseEntity<Void> deleteTil(@PathVariable Long tilId) {
+        tilService.deleteTil(tilId);
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/com/example/potatotilnewsfeed/domain/til/service/TilService.java
+++ src/main/java/com/example/potatotilnewsfeed/domain/til/service/TilService.java
@@ -52,4 +52,8 @@ public class TilService {
         til.setContent(requestDto.getContent());
         return tilRepository.save(til);
     }
+
+    public void deleteTil(Long tilId) {
+        tilRepository.deleteById(tilId);
+    }
 }

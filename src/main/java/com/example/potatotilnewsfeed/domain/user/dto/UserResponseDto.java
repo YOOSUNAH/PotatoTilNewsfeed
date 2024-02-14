aa3050faf6a4cd042c2ec8117c1fd50f92889d6c
YOOSUNAH <tjsdkyd4027@gmail.com>
2024-02-14 14:24:11 +0900

    :recycle: email도 response에서 보여주기

--- src/main/java/com/example/potatotilnewsfeed/domain/user/dto/UserResponseDto.java
+++ src/main/java/com/example/potatotilnewsfeed/domain/user/dto/UserResponseDto.java
@@ -3,6 +3,7 @@ package com.example.potatotilnewsfeed.domain.user.dto;
 import com.example.potatotilnewsfeed.domain.user.entity.User;
 import lombok.Getter;
 
+
 @Getter
 public class UserResponseDto {
     private final String nickname;

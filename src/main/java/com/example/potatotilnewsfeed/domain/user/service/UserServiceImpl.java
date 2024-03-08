b647e8fc55abb38bb51265cd99e4a722bd41aa3d
YOOSUNAH <tjsdkyd4027@gmail.com>
2024-03-08 15:48:35 +0900

    [스프링심화코드개선과제] 1. CustomException 정의 + 중복되는 부분 메서드로 분리 (리팩토링)

--- src/main/java/com/example/potatotilnewsfeed/domain/user/service/UserServiceImpl.java
+++ src/main/java/com/example/potatotilnewsfeed/domain/user/service/UserServiceImpl.java
@@ -7,8 +7,10 @@ import com.example.potatotilnewsfeed.domain.user.dto.UserResponseDto;
 import com.example.potatotilnewsfeed.domain.user.entity.User;
 import com.example.potatotilnewsfeed.domain.user.repository.TokenRepository;
 import com.example.potatotilnewsfeed.domain.user.repository.UserRepository;
-import java.util.NoSuchElementException;
+import com.example.potatotilnewsfeed.global.dto.NotFoundException;
+import com.example.potatotilnewsfeed.global.dto.DuplicatePasswordException;
 import com.example.potatotilnewsfeed.global.security.UserDetailsImpl;
+import java.util.NoSuchElementException;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -23,7 +25,7 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
-public class UserServiceImpl implements UserService{
+public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
     private final TokenRepository tokenRepository;
@@ -55,10 +57,9 @@ public class UserServiceImpl implements UserService{
             new NoSuchElementException("로그인되지 않은 토큰입니다."));
         token.setIsExpired(true);
     }
+
     public UserResponseDto getUser(@AuthenticationPrincipal UserDetailsImpl userDetails) {
-        Long userId = userDetails.getUser().getUserId();
-        User user = userRepository.findById(userId)
-            .orElseThrow(() -> new IllegalArgumentException("선택한 유저가 존재하지 않습니다."));
+        User user = validateUser(userDetails);
         return new UserResponseDto(user);
     }
 
@@ -66,11 +67,7 @@ public class UserServiceImpl implements UserService{
     @Transactional
     public UserResponseDto updateUser(@AuthenticationPrincipal UserDetailsImpl userDetails,
         UserRequestDto userRequestDto) {
-        // 토큰으로 id 가져오기
-        Long userId = userDetails.getUser().getUserId();
-        // DB에 접근
-        User user = userRepository.findById(userId)
-            .orElseThrow(() -> new IllegalArgumentException("선택한 유저가 존재하지 않습니다."));
+        User user = validateUser(userDetails);
         // 변경
         user.setNickname(userRequestDto.getNickname());
         user.setIntroduce(userRequestDto.getIntroduce());
@@ -78,35 +75,29 @@ public class UserServiceImpl implements UserService{
     }
 
     @Transactional
-    public UserResponseDto updatePassword(UserDetailsImpl userDetails, UserRequestDto userRequestDto) {
-        Long userId = userDetails.getUser().getUserId();
-        User user = userRepository.findById(userId)
-            .orElseThrow(() -> new IllegalArgumentException("선택한 유저가 존재하지 않습니다."));
-        // 기존 비밀번호 가져오기
-        String currentPassword = user.getPassword();
-        // 기존 비밀번호를 입력해주세요 (입력받은 값)
-        String password = userRequestDto.getPassword();
-        // 기존 비밀번호 확인
-        validatePassword(password, currentPassword);
+    public UserResponseDto updatePassword(UserDetailsImpl userDetails,
+        UserRequestDto userRequestDto) {
+        User user = validateUser(userDetails);
+        //  입력받은 값과 기존 비밀번호 확인
+        validatePassword(userRequestDto.getPassword(), user.getPassword());
         // 바꾸는 비밀번호
         String newPassword = userRequestDto.getNewPassword();
-        // 바꾸는 비밀번호
-        String checkPassword = userRequestDto.getCheckPassword();
         // 바꾸는 비밀번호 확인
-        validateNewPassword(newPassword, checkPassword);
+        validateNewPassword(newPassword, userRequestDto.getCheckPassword());
+
         // 바꾸는 비밀번호 인코딩해서 저장
         newPassword = passwordEncoder.encode(userRequestDto.getNewPassword());
         user.setPassword(newPassword);
+
         return new UserResponseDto(user);
     }
 
     @Transactional
     public void deleteUser(UserDetailsImpl userDetails, UserRequestDto userRequestDto) {
-        Long userId = userDetails.getUser().getUserId();
-        User user = userRepository.findById(userId)
-            .orElseThrow(() -> new IllegalArgumentException("선택한 유저가 존재하지 않습니다."));
+        User user = validateUser(userDetails);
         String password = userRequestDto.getPassword();
         String checkPassword = user.getPassword();
+
         // raw  , check (인코딩 된것)
         validatePassword(password, checkPassword);
         userRepository.delete(user);
@@ -120,8 +111,18 @@ public class UserServiceImpl implements UserService{
 
     private void validateNewPassword(String newPassword, String checkPassword) {
         if (!newPassword.equals(checkPassword)) {
-            throw new BadCredentialsException("기존 비밀번호와 동일합니다.");
+            throw new DuplicatePasswordException("기존 비밀번호와 동일합니다.");
         }
     }
 
+    private User validateUser(UserDetailsImpl userDetails) {
+        // 토큰으로 id 가져오기
+        Long userId = userDetails.getUser().getUserId();
+        // DB에 접근
+        User user = userRepository.findById(userId)
+            .orElseThrow(() -> new NotFoundException("선택한 유저가 존재하지 않습니다."));
+
+        return user;
+
+    }
 }
---
+++ src/main/java/com/example/potatotilnewsfeed/global/dto/DuplicatePasswordException.java
@@ -0,0 +1,10 @@
+package com.example.potatotilnewsfeed.global.dto;
+
+public class DuplicatePasswordException extends RuntimeException {
+
+    public DuplicatePasswordException(String message) {
+    super(message);
+    }
+
+
+}
